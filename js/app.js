3f0aeef2ce2941335a4391108765a66b512d1e01
eli bailey <baileyeli19@gmail.com>
2022-12-01 22:02:33 +0000

    updated about me

--- js/app.js
+++ js/app.js
@@ -1,60 +1,97 @@
 'use strict';
 
-let userQuestionOne = prompt( 'What is your name?');
+let userQuestionOne = prompt('What is your name?');
 alert(`welcome to my site ${userQuestionOne}!Please answer the following questions with yes/no or y/n`);
 
-let questionOneGuess = prompt( 'Do you I have siblings?').toLowerCase();
+let questionOneGuess = prompt('Do you I have siblings?').toLowerCase();
 
-if(questionOneGuess === 'yes' || questionOneGuess === 'y'){
-  alert ('You are correct!');
+if (questionOneGuess === 'yes' || questionOneGuess === 'y') {
+  alert('You are correct!');
 }
-else if (questionOneGuess === 'no' || questionOneGuess === 'n'){
-  alert ( 'Sorry you got it wrong');
-  
-}
-let userQuestionThree = prompt( 'Do I have children?').toLowerCase();
+else if (questionOneGuess === 'no' || questionOneGuess === 'n') {
+  alert('Sorry you got it wrong');
 
-if(userQuestionThree === 'yes' || userQuestionThree === 'y'){
-  alert ('Sorry you got it wrong ');
 }
-else{
-  alert ( 'Yes you are coreect!');
-}
+let userQuestionThree = prompt('Do I have children?').toLowerCase();
 
-let userQuestionGuess = prompt( 'Do I have any pets?').toLowerCase();
-  
-if(userQuestionGuess === 'yes' || userQuestionGuess === 'y'){
-  alert ('Sorry you got it wrong');
+if (userQuestionThree === 'yes' || userQuestionThree === 'y') {
+  alert('Sorry you got it wrong ');
 }
-else if (userQuestionGuess === 'no' || userQuestionGuess === 'n'){
-  alert ( 'You are correct!');
+else {
+  alert('Yes you are coreect!');
+
 }
 
-let fiveGuessQuestion = prompt('Do I own a instrument?').toLowerCase(); 
-if(fiveGuessQuestion === 'yes' || fiveGuessQuestion === 'y'){
-  alert ('You are correct!');
+let userQuestionGuess = prompt('Do I have any pets?').toLowerCase();
+
+if (userQuestionGuess === 'yes' || userQuestionGuess === 'y') {
+  alert('Sorry you got it wrong');
 }
-else if (fiveGuessQuestion === 'no' || fiveGuessQuestion === 'n'){
-  alert ( 'Sorry you got it wrong');
+else if (userQuestionGuess === 'no' || userQuestionGuess === 'n') {
+  alert('You are correct!');
+
 }
 
-let score = 0; 
+let fiveGuessQuestion = prompt('Do I own a instrument?').toLowerCase();
+if (fiveGuessQuestion === 'yes' || fiveGuessQuestion === 'y') {
+  alert('You are correct!');
 
+}
+else if (fiveGuessQuestion === 'no' || fiveGuessQuestion === 'n') {
+  alert('Sorry you got it wrong');
+}
+
+let score = 0;
 let nums = 3;
+for (let i = 0; i < 4; i++) {
 
-let userQuestionSix =prompt ('What number am I thinking of its a number between 1 and 7');
 
-while (nums < userQuestionSix){
-  userQuestionSix = prompt(`${userQuestionOne} try again you're too high`);
- nums--
-  if(userQuestionSix === nums){
-    alert (`${userQuestionOne} that'sa great guess, you are correct`);
-   
-    score++
+  let userQuestionSix = prompt('What number am I thinking of its a number between 1 and 7');
+
+  if (nums < userQuestionSix) {
+    alert(`${userQuestionOne} try again you're too high`);
   }
-  
+  else if (nums > userQuestionOne) {
+    alert(`${userQuestionOne} try again the number is too low`);
 
-  alert(`${userQuestionOne} you are correct`);
+  }
+  else if (userQuestionSix === nums) {
+    alert(`${userQuestionOne} that's a great guess, you are correct`);
+    score++;
+    break;
+
+  }
+  if (i === 3) {
+    alert(`${userQuestionOne} you are all out of chances the answer was ${nums}`);
+
+  }
+}
+
+
+
+for (let i = 0; i < 4; i++) {
+  alert(`${userQuestionOne} you are all out of chances the answer was ${nums}`);
+}
+
+let favMovie = ['superbad', 'harry potter', 'good will hunting', 'knives out', 'rush hour', 'hot tub time machine'];
+
+for (let i = 0; i < 6; i++) {
+  let questionSevenGuess = prompt(`${userQuestionOne} What do you think my favorite movie is?`).toLowerCase();
+  for (let j = 0; j < favMovie.length; j++) {
+    if (questionSevenGuess === favMovie[5]) {
+      alert('correct one of the funniest movies ever created!!!!')
+       if (questionSevenGuess === favMovie[0, || 1, || 2, || 3, || 4]) {
+        alert(`${userQuestionOne} all great movie but no. Please try again`);
+      }
+      else (questionSevenGuess === 6) {
+        alert(`${userQuestionOne} you are all out of tries. My favorite movie is ${favNovie[5]}`);
+      }
+      score++;
+      i = 6;
+      break;
+    }
+
+  }
 }
 
 
